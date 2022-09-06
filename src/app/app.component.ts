9a7c1d143c2560fe9e5e3b5e9f6a4224dd7472ee
Gustavo <gustavofrancodias@hotmail.com>
2022-09-06 10:59:08 -0300

    template usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fast-anki';
+  title = 'Quick Flash Card';
 }
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  faPlus = faPlus;
+}
